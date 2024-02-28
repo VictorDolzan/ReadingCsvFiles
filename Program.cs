a04c5445b86cf23667741bec5ab2a184c30b5a73
Victor Dolzan <victorddf5@gmail.com>
2024-02-28 11:56:13 -0300

    📜Implemented the Reading Csv file code properly.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿const string path = $@"C:\\Users\\victo\\Downloads\\sampleData.csv";
 
-Console.WriteLine("Hello, World!");
+var csvReader = new CsvReader().Read(path);
+
+Console.ReadKey();
+
+public class CsvReader
+{
+    public CsvData Read(string path)
+    {
+        const string Separator = ",";
+        using (var stremReader = new StreamReader(path))
+        {
+            var columns = stremReader.ReadLine().Split(Separator);
+
+            var rows = new List<string[]>();
+            
+            while (!stremReader.EndOfStream)
+            {
+                var cellsInRows = stremReader.ReadLine().Split(Separator);
+                rows.Add(cellsInRows);
+            }
+
+            return new CsvData(columns, rows);
+        }
+    }
+}
+
+public class CsvData
+{
+    public string[] Columns { get; }
+    public IEnumerable<string[]> Rows { get; }
+    
+    public CsvData(string[] columns, IEnumerable<string[]> rows)
+    {
+        Columns = columns;
+        Rows = rows;
+    }
+}
